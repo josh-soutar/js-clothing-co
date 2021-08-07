cf5b5b4d3d1af455c4591c246547a74f513ccc64
josh-soutar <josh.soutar@servicenow.com>
2021-08-07 11:15:34 +1000

    added cartIcon and cartDropdown components. Implemented cart-reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
+
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 // combineReducers is used to build the global state object from
 // a combination of all the other reducers
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
