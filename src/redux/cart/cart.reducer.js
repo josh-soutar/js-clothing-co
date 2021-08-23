cfdeaa3e6f56e858e878b3456b262efb9d34c052
Josh Soutar <joshsoutar@gmail.com>
2021-08-23 10:19:35 +1000

    Added add/remove item functionality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 //When the user first comes to the site, we want the cart dropdown to be hidden
 const INITIAL_STATE = {
@@ -19,6 +19,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((cartItem) => cartItem.id !== action.payload.id),
+      };
+
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       };
 
     default:
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,3 +9,17 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+  //If the item only has 1 quantity
+  if (existingCartItem && existingCartItem.quantity === 1) {
+    //Remove the item from the cart
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  //Otherwise, minus 1 from the quantity property
+  return cartItems.map((cartItem) => {
+    return cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem;
+  });
+};
