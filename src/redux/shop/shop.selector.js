f2c6ed36ef3597855dafd14bafc6ef2d2acbfd0f
Josh Soutar <joshsoutar@gmail.com>
2021-08-30 17:40:28 +1000

    Updated shop data to hash table. Updated shop selector

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -2,18 +2,14 @@ import { createSelector } from "reselect";
 
 const selectShop = (state) => state.shop;
 
-//Map URL parameter /shop/<collectionURLParam>
-//to state.shop.collection.id
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 export const selectShopCollections = createSelector([selectShop], (shop) => shop.collections);
 
+export const selectCollectionsForPreview = createSelector(
+  [selectShopCollections],
+  //Convert collections object into array
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollection = (collectionUrlParam) => {
-  return createSelector([selectShopCollections], (collections) => collections.find((collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]));
+  return createSelector([selectShopCollections], (collections) => collections[collectionUrlParam]);
 };
